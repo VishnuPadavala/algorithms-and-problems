4f12a964e15b54b4da0c5fd10820ed92601cd10e
Vishnu <vishnupadavala@gmail.com>
2025-08-24 22:50:05 +0530

    Update segmenttree.cpp

--- segmenttree.cpp
+++ segmenttree.cpp
@@ -1,47 +1,66 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class segmentTree{
-	private:
-		vector<int>segtree;
-	public:
-		segmentTree(int n){
-				segtree.resize(4*n);
-	}
-	//adding the element in a tree from
-	void add(int index,int start,int end,vector<int>&arr)
-	{
-		//adding the number into segtree tree by base 
-		if(start==end){
-			segtree[index]=arr[end];
-		}
-		int mid=(start+end)/2;
-		add(2*index+1,start,mid,arr);
-		add(2*index+2,mid+1,end,arr);
-		segtree[index]=segtree[2*index+1]+segtree[2*index+2];
-		
-	}
-	//rangsum return the sum
-	int rangesum(int ql,int qr,int start,int end,int index)
-	{
-		//there is no overlap in the arry
-		if(ql>end||qr<start)
-		{
-			return -1;          
-		}
-	}
-	
+
+class segmentTree {
+private:
+    vector<int> segtree;
+    int n;
+
+public:
+    segmentTree(int n) {
+        this->n = n;
+        segtree.resize(4 * n);
+    }
+
+    // Build the segment tree
+    void build(int index, int start, int end, vector<int>& arr) {
+        if (start == end) {
+            segtree[index] = arr[start];
+            return;
+        }
+        int mid = (start + end) / 2;
+        build(2 * index + 1, start, mid, arr);
+        build(2 * index + 2, mid + 1, end, arr);
+        segtree[index] = segtree[2 * index + 1] + segtree[2 * index + 2];
+    }
+
+    // Range Sum Query
+    int rangesum(int ql, int qr, int start, int end, int index) {
+        // no overlap
+        if (qr < start || ql > end) {
+            return 0;
+        }
+        // total overlap
+        if (ql <= start && end <= qr) {
+            return segtree[index];
+        }
+        // partial overlap
+        int mid = (start + end) / 2;
+        int left = rangesum(ql, qr, start, mid, 2 * index + 1);
+        int right = rangesum(ql, qr, mid + 1, end, 2 * index + 2);
+        return left + right;
+    }
 };
-int main()
-{
-	segmentTree s(5);
-	vector<int>arr;
-	int n;
-	cin>>n;
-	for(int i=0;i<n;i++){
-		cin>>arr[i];
-	}
-	add(0,0,n-1,arr);
-	int l,r;
-	cin>>l>>r;
-	rangesum(l,r,0,n-1,0);
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    segmentTree s(n);
+    s.build(0, 0, n - 1, arr);
+
+    int l, r;
+    cout << "Enter query range (l r): ";
+    cin >> l >> r;
+
+    cout << "Range sum = " << s.rangesum(l, r, 0, n - 1, 0) << endl;
+
+    return 0;
 }
